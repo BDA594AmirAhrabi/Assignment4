3091d91bd1ee214b79890057eaf6ff7ed6e07c52
BDA594AmirAhrabi <aahrabi8122@sdsu.edu>
2025-06-21 09:29:21 -0700

    Add files via upload

---
+++ Routes_10_16m.js
@@ -0,0 +1,16 @@
+var routes_10_16m = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [-76.609, 39.33],
+          [-76.610, 39.34],
+          [-76.611, 39.35]
+        ]
+      }
+    }
+  ]
+};
---
+++ Routes_6_9m.js
@@ -0,0 +1,16 @@
+var routes_6_9m = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [-76.609, 39.30],
+          [-76.610, 39.31],
+          [-76.611, 39.32]
+        ]
+      }
+    }
+  ]
+};
